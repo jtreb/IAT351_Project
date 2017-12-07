cd8ab4148c88d9f114610240337e3983b6b1e188
Unknown <yla312@sfu.ca>
2017-12-06 16:33:13 -0800

    tooltip positioning update

--- js/major.js
+++ js/major.js
@@ -5,10 +5,15 @@ function showiat100(){
 
   var modal = document.getElementById('iat100modal');
 
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
+  // modal.style.top = y+"px";
+  // modal.style.left = x+"px";
+  modal.style.position = "absolute";
+  // modal.style.top = "50%";
+  // modal.style.right = "0";
+  // modal.style.transform = "translate(0, -25%)";
   modal.style.display = "block";
-
+  // modal.style.width = "50%";
+  modal.style.position = "fixed";
 }
 
 function showiat102(){
@@ -16,11 +21,9 @@ function showiat102(){
   var y = event.pageY;
 
   var modal = document.getElementById('iat102modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
+  modal.style.position = "absolute";
+  modal.style.position = "fixed";
   modal.style.display = "block";
-
 }
 
 function showiat103(){
@@ -28,11 +31,9 @@ function showiat103(){
   var y = event.pageY;
 
   var modal = document.getElementById('iat103modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 
 function showiat106(){
@@ -40,363 +41,297 @@ function showiat106(){
   var y = event.pageY;
 
   var modal = document.getElementById('iat106modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat167(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat167modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcmpt120(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cmpt120modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcmpt1202(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cmpt120modal2');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcmpt125(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cmpt125modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcmpt126(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cmpt126modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcmpt128(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cmpt128modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcmpt166(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cmpt166modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showmacm101(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('macm101modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat201(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat201modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat202(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat202modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat222(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat222modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat233(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat233modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat235(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat235modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat265(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat265modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat267(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat267modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat333(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat333modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat334(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat334modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat355(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat355modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat381(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat381modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat445(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat445modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcmpt320(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cmpt320modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcmpt(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cmptmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showensc(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('enscmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showstat(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('statmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showphys(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('physmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iatmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showkin(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('kinmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showkin2(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('kinmodal2');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showmssc(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('msscmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showmath(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('mathmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showmacm(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('macmmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcogs(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cogsmodal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showpsyc(){
   var x = event.pageX;
@@ -404,121 +339,99 @@ function showpsyc(){
 
   var modal = document.getElementById('psycmodal');
 
-    console.log(modal);
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+    console.log(modal);modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcmpt2(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cmptmodal2');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showensc2(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('enscmodal2');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showcogs381(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('cogs381modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showpsyc362(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('psyc362modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showpsyc352(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('psyc352modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat351(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat351modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat352(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat352modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat410(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat410modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat452(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat452modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat455(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat455modal');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 
 function showiat3332(){
@@ -526,55 +439,45 @@ function showiat3332(){
   var y = event.pageY;
 
   var modal = document.getElementById('iat333modal2');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat3342(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat334modal2');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat3552(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat355modal2');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat3812(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat381modal2');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 function showiat4452(){
   var x = event.pageX;
   var y = event.pageY;
 
   var modal = document.getElementById('iat445modal2');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 
 function showiat3553(){
@@ -582,11 +485,9 @@ function showiat3553(){
   var y = event.pageY;
 
   var modal = document.getElementById('iat355modal3');
-
-  modal.style.top = y+"px";
-  modal.style.left = x+"px";
-  modal.style.display = "block";
-
+modal.style.position = "absolute";
+modal.style.position = "fixed";
+modal.style.display = "block";
 }
 
 
